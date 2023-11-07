0bf031568df5f25d6f16448b8b6c5745d9e78e0f
Valery Zinchenko <minicablestone@gmail.com>
2023-11-07 11:17:58 +0300

    update build

---
+++ rollup.config.js
@@ -0,0 +1,39 @@
+import { defineConfig } from "rollup"
+import { swc } from "rollup-plugin-swc3"
+
+const baseConfig = defineConfig({
+  treeshake: true,
+  external: [/react.*/, /@swc\/helpers\/.*/, /lodash.*/, "zod"],
+  input: "./src/index.ts",
+  output: {
+    format: "esm",
+    dir: "dist",
+    sourcemap: true,
+    preserveModules: false,
+    exports: "auto"
+  },
+})
+const config = defineConfig([
+  {
+    ...baseConfig,
+    plugins: [
+      swc({
+        sourceMaps: true,
+        jsc: {
+          loose: true,
+          externalHelpers: false,
+          parser: {
+            syntax: "typescript",
+            dynamicImport: true
+          },
+          minify: {
+            sourceMap: true
+          }
+        },
+        minify: true
+      })
+    ]
+  }
+])
+
+export default config
